cc6b3cdb764ae59765b4c7861f17fb21f8b1f49d
wangdavidhao <wangdavid1999@gmail.com>
2020-10-06 21:30:59 +0200

    Question 4

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,7 +3,8 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL2 = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
@@ -12,7 +13,7 @@ class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
     if(city === undefined){
-      city = "paris";
+      city = "nantes";
     }
     this.city = city;
   }
@@ -21,10 +22,19 @@ class API_WEATHER{
   // Retourne une promise
   fetchTodayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+    .get(`${API_URL2}?q=${this.city}&lang=fr&units=metric&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
+
+  getThreeDayForecast(){
+    return axios
+    .get(`${API_URL}?q=${this.city}&lang=fr&units=metric&cnt=3&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+
+  }
+ 
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
-
-// Fonction appelée lors du click du bouton
 function start() {
 
   let apiWeather = null;
   let city = document.getElementById('city-input').value;
-  console.log(city);
- 
+
   if(city==''){
     // Création de l'objet apiWeather
     apiWeather = new API_WEATHER();
-    // Appel de la fonction fetchTodayForecast
+    
   }else{
     apiWeather = new API_WEATHER(city);
   }
   
-
+  // Appel de la fonction fetchTodayForecast
   apiWeather
     .fetchTodayForecast()
     .then(function(response) {
@@ -39,6 +36,44 @@ function start() {
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
-    });
+  });
+
+
+  apiWeather
+  .getThreeDayForecast()
+  .then(function(response) {
+    // Récupère la donnée d'une API
+    const data = response.data.list;
+
+    let i=1;
+
+    while(i<=data.length){
+
+      data.map(element =>{
+        
+        console.log('element' , element);
+        // On récupère l'information principal
+        const main = element.weather[0].main;
+        const description = element.weather[0].description;
+        const temp = element.temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(element.weather[0].icon);
+        // Modifier le DOM
+
+        document.getElementById('today-forecast-main-next' +(i).toString()).innerHTML = main;
+        document.getElementById('today-forecast-more-info-next'+(i).toString()).innerHTML = description;
+        document.getElementById('icon-weather-container-next'+(i).toString()).innerHTML = icon;
+        document.getElementById('today-forecast-temp-next'+(i).toString()).innerHTML = `${temp}°C`;
+        i++;
+
+      })
+
+    }
+    
+  })
+  .catch(function(error) {
+    // Affiche une erreur
+    console.error(error);
+  });
+      
 }
 
