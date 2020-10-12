6684cf9416d73c6fcd02cb0a7b7d7d2dd11c298f
wangdavidhao <wangdavid1999@gmail.com>
2020-10-12 21:11:26 +0200

    bug select option

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function generateDays(numberDays) {
+  
+  let x = numberDays;
+  console.log(x);
 
   let apiWeather = null;
   let city = document.getElementById('city-input').value;
+  const options = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
 
   if(city==''){
     // Création de l'objet apiWeather
@@ -11,85 +15,89 @@ function generateDays(numberDays) {
     apiWeather = new API_WEATHER(city);
   }
 
-  // Appel de la fonction fetchTodayForecast
-  apiWeather
-    .fetchTodayForecast()
+  
+  let select  = document.getElementById('daysOption');
+  
+  if(x === 4){
+
+
+    // Appel de la fonction fetchTodayForecast
+    apiWeather
+      .fetchTodayForecast()
+      .then(function(response) {
+        // Récupère la donnée d'une API
+        const data = response.data;
+
+        // On récupère l'information principal
+        const main = data.weather[0].main;
+        const description = data.weather[0].description;
+        const temp = data.main.temp;
+        const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+
+
+        // Modifier le DOM
+        document.getElementById('city').innerHTML = 'Aujourd\'hui - ' + data.name;
+        document.getElementById('today-forecast-main').innerHTML = main;
+        document.getElementById('today-forecast-more-info').innerHTML = description;
+        document.getElementById('icon-weather-container').innerHTML = icon;
+        document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+        
+      })
+      .catch(function(error) {
+        // Affiche une erreur
+        console.error(error);
+    });
+
+
+    apiWeather
+    .getThreeDayForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
-      const data = response.data;
+      const data = response.data.list;
+      let i=1;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
-
-
-      // Modifier le DOM
-      document.getElementById('city').innerHTML = 'Aujourd\'hui - ' + data.name;
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+        data.map(element =>{
+          
+          // On récupère l'information principal
+          const main = element.weather[0].main;
+          const description = element.weather[0].description;
+          const temp = element.temp.day;
+          const icon = apiWeather.getHTMLElementFromIcon(element.weather[0].icon);
+          const dt = element.dt;
+          const date = new Date(dt*1000);
+          
+          // Modifier le DOM
+          document.getElementById(`today-forecast-main-next${i.toString()}`).innerHTML = main;
+          document.getElementById(`today-forecast-more-info-next${i.toString()}`).innerHTML = description;
+          document.getElementById(`icon-weather-container-next${i.toString()}`).innerHTML = icon;
+          document.getElementById(`today-forecast-temp-next${i.toString()}`).innerHTML = `${temp}°C`;
+          document.getElementById(`date${i.toString()}`).innerHTML = date.toLocaleString("fr-FR", options);
+          i++;
+          for(let j=4;j<15;j++){
+              document.getElementById(`nextDay${j.toString()}`).style.display = 'none';
+          }
+        })
       
     })
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
-  });
+    });
 
-
-  apiWeather
-  .getThreeDayForecast()
-  .then(function(response) {
-    // Récupère la donnée d'une API
-    const data = response.data.list;
-    let i=1;
+  }else if(x != undefined){
     
-    while(i<=data.length){
-
-      data.map(element =>{
-        
-        // On récupère l'information principal
-        const main = element.weather[0].main;
-        const description = element.weather[0].description;
-        const temp = element.temp.day;
-        const icon = apiWeather.getHTMLElementFromIcon(element.weather[0].icon);
-        const dt = element.dt;
-        const date = new Date(dt*1000);
-        // Modifier le DOM
-
-        document.getElementById(`today-forecast-main-next${i.toString()}`).innerHTML = main;
-        document.getElementById(`today-forecast-more-info-next${i.toString()}`).innerHTML = description;
-        document.getElementById(`icon-weather-container-next${i.toString()}`).innerHTML = icon;
-        document.getElementById(`today-forecast-temp-next${i.toString()}`).innerHTML = `${temp}°C`;
-        document.getElementById(`date${i.toString()}`).innerHTML = date.toDateString();
-        i++;
-        for(let j=4;j<15;j++){
-            document.getElementById(`nextDay${j.toString()}`).style.display = 'none';
-        }
-      })
-
-    }
-    
-  })
-  .catch(function(error) {
-    // Affiche une erreur
-    console.error(error);
-  });
-
-  if(numberDays != undefined){
 
     apiWeather
     .getTwoWeekForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data.list;
+      
       let i=1;
 
-        for(let j=0;j<numberDays;j++){
+        for(let j=0;j<x;j++){
 
-          for(let k=4;k<=numberDays;k++){
+          for(let k=4;k<=x;k++){
               document.getElementById(`nextDay${k.toString()}`).style.display = 'unset';
           }
           
@@ -102,16 +110,17 @@ function generateDays(numberDays) {
           const date = new Date(dt*1000);
 
           // Modifier le DOM
+          document.getElementById('city').innerHTML = 'Aujourd\'hui - ' + response.data.city.name;
           document.getElementById(`today-forecast-main-next${i.toString()}`).innerHTML = main;
           document.getElementById(`today-forecast-more-info-next${i.toString()}`).innerHTML = description;
           document.getElementById(`icon-weather-container-next${i.toString()}`).innerHTML = icon;
           document.getElementById(`today-forecast-temp-next${i.toString()}`).innerHTML = `${temp}°C`;
-          document.getElementById(`date${i.toString()}`).innerHTML = date.toDateString();
+          document.getElementById(`date${i.toString()}`).innerHTML = date.toLocaleString("fr-FR", options);
           
           i++;
 
         }
-      
+    
     })
     .catch(function(error) {
       // Affiche une erreur
@@ -132,7 +141,10 @@ const moreDays = () => {
     }else if(select.value==='14 jours'){
       generateDays(14);
     }else if(select.value==='4 jours'){
-      generateDays();
+      generateDays(4);
     }
   })
+
+
 }
+
